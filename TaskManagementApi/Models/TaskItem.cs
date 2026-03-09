26c968cd167a1fd36f52aa7a8ed629b95b0d9405
Tento <regartento@gmail.com>
2026-03-09 14:45:00 +0700

    add task and user model

---
+++ TaskManagementApi/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagementApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool isCompleted { get; set; } = false;
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public User User { get; set; } = null!;
+    }
+}
